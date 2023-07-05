72617025811dce843b6d4310a145c1d448ac68b1
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-07-05 19:04:35 +0100

    complete header and css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -153,6 +153,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join( BASE_DIR, 'static' ), )
+
+# although normally we would also want to supply a static route setting here 
+# for Django's collectstatic utility to work but I'm not going to do that 
+# because it will interfere with setting up Amazon Web Services later on.
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join( BASE_DIR, 'media' )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
