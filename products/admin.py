5fc8036f44b7dcb3e0ebb5602c8c04d18f31b2b8
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-07-06 11:31:59 +0100

    customise admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+        )
+    
+    # As it's possible to sort by multiple fields, we need to create a tuple
+    # To reverse the order, add a minus sign before the field name
+    ordering = ('sku',) 
+    
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        )
+    
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
