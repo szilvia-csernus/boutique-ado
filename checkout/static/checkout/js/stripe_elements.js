66c0d52518e36604c273801633a5d13ba4999ebd
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-07-28 20:38:32 +0100

    add checkout form data caching in payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -54,47 +54,50 @@ form.addEventListener('submit', function (ev) {
 	$('#payment-form').fadeToggle(100);
 	$('#loading-overlay').fadeToggle(100);
 
-	// var saveInfo = Boolean($('#id-save-info').attr('checked'));
-	// // Form using {% csrf_token %} in the form
-	// var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
-	// var postData = {
-	// 	csrfmiddlewaretoken: csrfToken,
-	// 	client_secret: clientSecret,
-	// 	save_info: saveInfo,
-	// };
-	// var url = '/checkout/cache_checkout_data/';
+	var saveInfo = Boolean($('#id-save-info').attr('checked'));
+	// Form using {% csrf_token %} in the form
+	var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+	var postData = {
+		csrfmiddlewaretoken: csrfToken,
+		client_secret: clientSecret,
+		save_info: saveInfo,
+	};
+	var url = '/checkout/cache_checkout_data/';
 
-	// $.post(url, postData)
-	// 	.done(function () {
+	$.post(url, postData)
+		.done(function () {
 			stripe
 				.confirmCardPayment(clientSecret, {
 					payment_method: {
 						card: card,
-						// billing_details: {
-						// 	name: $.trim(form.full_name.value),
-						// 	phone: $.trim(form.phone_number.value),
-						// 	email: $.trim(form.email.value),
-						// 	address: {
-						// 		line1: $.trim(form.street_address1.value),
-						// 		line2: $.trim(form.street_address2.value),
-						// 		city: $.trim(form.town_or_city.value),
-						// 		country: $.trim(form.country.value),
-						// 		state: $.trim(form.county.value),
-						// 	},
-						// },
+						billing_details: {
+							name: $.trim(form.full_name.value),
+							phone: $.trim(form.phone_number.value),
+							email: $.trim(form.email.value),
+							// there is no postal code in the form because it's coming from the billing address
+							// which is coming from the stripe payment and stripe would overwrite it anyway
+							// if we tried to add it
+							address: {
+								line1: $.trim(form.street_address1.value),
+								line2: $.trim(form.street_address2.value),
+								city: $.trim(form.town_or_city.value),
+								country: $.trim(form.country.value),
+								state: $.trim(form.county.value),
+							},
+						},
 					},
-					// shipping: {
-					// 	name: $.trim(form.full_name.value),
-					// 	phone: $.trim(form.phone_number.value),
-					// 	address: {
-					// 		line1: $.trim(form.street_address1.value),
-					// 		line2: $.trim(form.street_address2.value),
-					// 		city: $.trim(form.town_or_city.value),
-					// 		country: $.trim(form.country.value),
-					// 		postal_code: $.trim(form.postcode.value),
-					// 		state: $.trim(form.county.value),
-					// 	},
-					// },
+					shipping: {
+						name: $.trim(form.full_name.value),
+						phone: $.trim(form.phone_number.value),
+						address: {
+							line1: $.trim(form.street_address1.value),
+							line2: $.trim(form.street_address2.value),
+							city: $.trim(form.town_or_city.value),
+							country: $.trim(form.country.value),
+							postal_code: $.trim(form.postcode.value),
+							state: $.trim(form.county.value),
+						},
+					},
 				})
 				.then(function (result) {
 					if (result.error) {
@@ -115,9 +118,9 @@ form.addEventListener('submit', function (ev) {
 						}
 					}
 				});
-		// })
-		// .fail(function () {
-		// 	// just reload the page, the error will be in django messages
-		// 	location.reload();
-		// });
+		})
+		.fail(function () {
+			// just reload the page, the error will be in django messages
+			location.reload();
+		});
 });
