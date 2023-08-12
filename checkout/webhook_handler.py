7a546597f419afee52bac4bc49ae4f16ec108431
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-08-12 11:55:03 +0100

    add order confirmation emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -51,7 +51,6 @@ class StripeWH_Handler:
         pid = intent.id
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
-        
         # Get the Charge object
         stripe_charge = stripe.Charge.retrieve(
             intent.latest_charge
@@ -121,7 +120,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
-                    # user_profile=profile,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
