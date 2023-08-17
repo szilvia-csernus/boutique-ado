015fffced78f3a56b64696a9021b989677820776
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-08-17 15:42:01 +0100

    add SERVER_EMAIL to settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -240,4 +240,4 @@ else:
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
     EMAIL_PORT = 587
-    
+    SERVER_EMAIL = os.environ.get("SERVER_EMAIL")
