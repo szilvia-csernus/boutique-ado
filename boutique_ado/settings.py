82e0f7d85f23d8745b2c54c6d10f51d3eeaced09
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-08-17 12:41:44 +0100

    fix DATABASE_URL

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -134,15 +134,16 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 if 'DATABASE_URL' in os.environ: 
     DATABASES = {
+        'default': dj_database_url.parse( os.environ.get('DATABASE_URL', '') )
+    }
+else:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse( os.environ.get('DATABASE_URL', '') )
-    }
+    
 
 
 # Password validation
