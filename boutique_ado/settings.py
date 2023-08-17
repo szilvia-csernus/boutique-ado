468acb4894ac83a3ebb73d229512971e23f1e1bd
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-08-17 14:59:46 +0100

    turn on email verification

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -203,7 +203,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+# ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 if 'USE_AWS' in os.environ:
     # Cache control
