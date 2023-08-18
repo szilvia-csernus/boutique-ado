3e7b569f8e84a6b0b4aa1249542a0a64e87234f7
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-08-18 15:06:24 +0100

    fix linting errors

--- products/views.py
+++ products/views.py
@@ -9,13 +9,13 @@ from .forms import ProductForm
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-    
+
     products = Product.objects.all()
     query = None
     categories = None
-    sort= None
+    sort = None
     direction = None
-    
+
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
@@ -23,66 +23,70 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-                
+
             if sortkey == 'category':
                 sortkey = 'category__name'
-                
+
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     # '-' means descending
                     sortkey = f'-{sortkey}'
-                    
+
             products = products.order_by(sortkey)
-                
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-        
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You did not enter any search criteria!")
+                messages.error(request,
+                               "You did not enter any search criteria!")
                 return redirect(reverse('products'))
-            
+
             # 'i' in 'icontains' means case insensitive
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = (Q(name__icontains=query) |
+                       Q(description__icontains=query))
             products = products.filter(queries)
-            
+
     current_sorting = f'{sort}_{direction}'
-    
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
     }
-    
+
     return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
-    
+
     product = get_object_or_404(Product, pk=product_id)
-    
+
     context = {
         'product': product,
     }
-    
+
     return render(request, 'products/product_detail.html', context)
+
 
 @login_required
 def add_product(request):
     """ Add a product to the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can manage products.')
+        messages.error(request,
+                       'Sorry, only store owners can manage products.')
         return redirect(reverse('home'))
-    
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
-        
+
         if form.is_valid():
             # Save the form to the database
             product = form.save()
@@ -92,28 +96,32 @@ def add_product(request):
             return redirect(reverse('product_detail', args=[product.id]))
         else:
             # Display an error message
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
-    
+            messages.error(request,
+                           """Failed to add product.
+                           Please ensure the form is valid.""")
+
     form = ProductForm()
     template = 'products/add_product.html'
     context = {
         'form': form,
     }
-    
+
     return render(request, template, context)
+
 
 @login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can manage products.')
+        messages.error(request,
+                       'Sorry, only store owners can manage products.')
         return redirect(reverse('home'))
-    
+
     product = get_object_or_404(Product, pk=product_id)
-    
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
-        
+
         if form.is_valid():
             # Save the form to the database
             form.save()
@@ -123,24 +131,28 @@ def edit_product(request, product_id):
             return redirect(reverse('product_detail', args=[product.id]))
         else:
             # Display an error message
-            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
-    
+            messages.error(request,
+                           """Failed to update product.
+                           Please ensure the form is valid.""")
+
     form = ProductForm(instance=product)
     template = 'products/edit_product.html'
     context = {
         'form': form,
         'product': product,
     }
-    
+
     return render(request, template, context)
+
 
 @login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can manage products.')
+        messages.error(request,
+                       'Sorry, only store owners can manage products.')
         return redirect(reverse('home'))
-    
+
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
